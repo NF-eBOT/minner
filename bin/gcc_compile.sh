7107a47a305bd775327d2cd89aba13f586fc6772
Jonatas Freitas <jonatasfreitasv@gmail.com>
2017-02-20 02:24:40 -0300

    Same refactor in bin

--- bin/gcc_compile.sh
+++ bin/gcc_compile.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+echo "GCC compile started !"
 g++ -std=c++14 src/main.cpp includes/*.cpp includes/parsers/*.cpp -w -lcurl -o build/scrapers_cpp.out
+echo "GCC compile end, see BUILD folder !"
