882f9cfdfd4d99495e6d9090dd8a5b62dd2634c3
Jonatas Freitas <jonatasfreitasv@gmail.com>
2017-03-04 03:04:07 -0300

    update cmae

---
+++ scripts/cmake_clear.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Compiled files
+rm minner
+rm minner.cbp
+
+# cmake files
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm Makefile
+
+# cmake folders
+rm -rf cmake-build-debug/
+rm -rf CMakeFiles/
